3f0a691778d0a0818cb0778d91a21feb66fc3988
Evans A. Stepanov <evans@stepanov.email>
2021-03-27 01:09:18 -0400

    fix ad styles

--- src/components/ads/LeaderboardAd.tsx
+++ src/components/ads/LeaderboardAd.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import "twin.macro";
 
 const LeaderboardAd: React.FC = () => {
   return (
@@ -7,13 +8,22 @@ const LeaderboardAd: React.FC = () => {
       <Helmet>
         <script async src="https://cdn.contextcue.com/adsbycontextcue.js" />
       </Helmet>
-      <div tw="flex justify-center items-center bg-gray-100 my-5">
-        <ins
-          className="adsbycontextcue"
-          data-cc-site="d1ab5b1a-222c-4977-aee0-01521e92ffcb"
-          data-cc-slot="9u0dHZJqz"
+      <div tw="flex justify-center items-center py-5">
+        <div
+          tw="relative border border-gray-200 bg-white"
           style={{ width: 782, height: 90 }}
-        />
+        >
+          <ins
+            className="adsbycontextcue"
+            data-cc-site="d1ab5b1a-222c-4977-aee0-01521e92ffcb"
+            data-cc-slot="9u0dHZJqz"
+            style={{ width: 782, height: 90 }}
+            tw="absolute inset-0"
+          />
+          <div tw="absolute inset-0 flex justify-center items-center text-xl font-bold tracking-wide text-gray-700">
+            Advertisements!
+          </div>
+        </div>
       </div>
     </>
   );
